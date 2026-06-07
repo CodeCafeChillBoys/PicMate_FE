b77cf7e89fec962863b1fff014d2e9e24e278849
VoVanCuong <cuongcon2k04@gmail.com>
2026-06-07 14:29:02 +0700

    add url BE deloyed

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7123';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://picmate-api-latest.onrender.com';
 
 const http = axios.create({
   baseURL: API_BASE_URL,
